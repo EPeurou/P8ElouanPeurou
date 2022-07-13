4060219d45f6568105ded824ddba62c56b9a8c64
elouan <elouan.peurou@gmail.com>
2022-07-13 11:19:42 +0200

    logout ok and add some nav buttons

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,44 +9,17 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 
 /**
  * @Route("/security")
  */
 class SecurityController extends AbstractController
 {
-    use TargetPathTrait;
-
-    public const LOGIN_ROUTE = 'login_check';
-
-    private $urlGenerator;
     
-    public function __construct(UserRepository $userRepository, RouterInterface $router, UrlGeneratorInterface $urlGenerator)
-    {
-        // $this->userRepository = $userRepository;
-        // $this->router = $router;
-        $this->urlGenerator = $urlGenerator;
-    }
-
-    public function supports(Request $request): ?bool
-    {
-        return ($request->getPathInfo() === '/login_check' && $request->isMethod('POST'));
-    }
 
     /**
      * @Route("/login", name="login")
@@ -54,59 +27,47 @@ class SecurityController extends AbstractController
     public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
         // $authenticationUtils = $this->container->get('security.authentication_utils');
-
+        $user = $this->getUser();
+        if ($user != null){
+            // dd("ok");
+            return $this->redirectToRoute('task_list', [], Response::HTTP_SEE_OTHER);
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
-        // $lastUsername = $authenticationUtils->getLastUsername();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('securityfirst/login.html.twig', array(
-            'last_username' => "tst",
+            'last_username' => $lastUsername,
             'error'         => $error,
         ));
     }
 
-    /**
-     * @Route("/login_check", name="login_check", methods={"POST"})
-     */
-    public function loginCheck(Request $request): PassportInterface
-    {
-        // dd("test");
-        $password = $request->request->get('password');
-        // dd("test");
-        $username = $request->request->get('username');
+    // /**
+    //  * @Route("/login_check", name="login_check", methods={"POST"})
+    //  */
+    // public function loginCheck(Request $request): PassportInterface
+    // {
+    //     // dd("test");
+    //     $password = $request->request->get('password');
+    //     // dd("test");
+    //     $username = $request->request->get('username');
         
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
+    //     $request->getSession()->set(Security::LAST_USERNAME, $username);
 
-        return new Passport(
-            new UserBadge($username),
-            new PasswordCredentials($request->request->get('password')),
-            [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-            ]
-        );
-    }
+    //     return new Passport(
+    //         new UserBadge($username),
+    //         new PasswordCredentials($request->request->get('password')),
+    //         [
+    //             new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+    //         ]
+    //     );
+    // }
 
     /**
      * @Route("/logout", name="logout")
      */
     public function logoutCheck()
     {
-        // This code is never executed.
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        dd("success");
-        return new RedirectResponse(
-            $this->router->generate('main')
-        );
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    { 
-        dd("error");
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-        return new RedirectResponse(
-            $this->router->generate('login')
-        );
-    }
 }
